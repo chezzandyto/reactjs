feda111c9187b3eba90d568b8f368393f0d16b64
chezzandyto <andres_toapanta@hotmail.com>
2021-07-22 23:40:29 -0500

    clase 10

--- src/index.js
+++ src/index.js
@@ -13,20 +13,23 @@ import ReactDOM from 'react-dom';
 // const element = React.createElement('h1', {}, `Hola, soy ${name}`);
 // const jsx = <h1>Hola soy, {undefined}</h1>;
 
-const jsx = (
-  <div>
-    <h1>Hola, soy Richard</h1>
-    <p>Soy ingeniero frontend.</p>
-  </div>
-);
+// const jsx = (
+//   <div>
+//     <h1>Hola, soy Richard</h1>
+//     <p>Soy ingeniero frontend.</p>
+//   </div>
+// );
 
-const element = React.createElement(
-  'div',
-  {},
-  React.createElement('h1', {}, 'Hola, soy Richard'),
-  React.createElement('p', {}, 'Soy ingeniero de la web.')
-);
+// const element = React.createElement(
+//   'div',
+//   {},
+//   React.createElement('h1', {}, 'Hola, soy Richard'),
+//   React.createElement('p', {}, 'Soy ingeniero de la web.')
+// );
+
+import Badge from './components/Badge';
+
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(<Badge />, container);
